dc6d61f258caee9bab616897a604194dce3553ee
Muhozi Emery <muhozie@gmail.com>
2018-03-06 14:20:39 +0300

    [Feature #155731700] Implement registration

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, flash, session, redirect, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
 from api.api import api
 app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(api)
